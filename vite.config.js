64f0e4a8743a816514d1a2b739ec120f2f2c516c
ahmed ajitena <pacsizzo07@gmail.com>
2025-05-12 00:49:25 +0300

    second commit

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 // vite.config.js
 import { defineConfig } from "vite";
 
+
+
 export default defineConfig({
-  base: "/deploy-vite-testing",
-});
+  base: '/deploy-vite-testing/', // <- very important for GitHub Pages!
+  build: {
+    outDir: 'dist',
+  }
+})
